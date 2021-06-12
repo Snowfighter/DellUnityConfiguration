8f43694304ceae0334c7ec66edc0bcbada2032b3
Snowfighter <chernayev.sergey.work@gmail.com>
2021-06-12 14:01:58 +0300

    Added test code

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import subprocess
-ip = raw_input('Enter ip addr: ')
-bashCommand = 'uemcli -d ' + ip + ' /net/ntp/server show detail'
+serverIP = raw_input('Enter server ip addr: ')
+ntpIP = raw_input('Enter server ip addr: ')
+bashCommand = 'uemcli -d ' + ip + ' /net/ntp/server create -server ' + ntpIP
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
 print('Output: {output}'.format(output=output))
+print(type(output))
+if 'Operation completed successfully' in output:
+    print('Success')
 print('Error: {error}'.format(error=error))
